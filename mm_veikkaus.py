ac30cc120cf5d2f3d93e6d84fb6642a934b1aca1
mrksfbrts <mrksfabritius@gmail.com>
2026-07-09 02:18:30 +0300

    Add files via upload

--- mm_veikkaus.py
+++ mm_veikkaus.py
@@ -262,7 +262,7 @@ matches = [
     {"id":97, "date":"2026-07-9", "time":"23:00", "home":"Ranska", "away":"Marokko", "round":"Puolivälierä", "double_points": False},
     {"id":98, "date":"2026-07-10", "time":"22:00", "home":"Espanja", "away":"Belgia", "round":"Puolivälierä", "double_points": False},
     {"id":99, "date":"2026-07-12", "time":"00:00", "home":"Norja", "away":"Englanti", "round":"Puolivälierä", "double_points": False},
-    {"id":100, "date":"2026-07-12", "time":"04:00", "home":"Argentiina", "Sveitsi":"TBA", "round":"Puolivälierä", "double_points": False},
+    {"id":100, "date":"2026-07-12", "time":"04:00", "home":"Argentiina", "away":"Sveitsi", "round":"Puolivälierä", "double_points": False},
     {"id":101, "date":"2026-07-14", "time":"22:00", "home":"TBA", "away":"TBA", "round":"Välierä", "double_points": False},
     {"id":102, "date":"2026-07-15", "time":"22:00", "home":"TBA", "away":"TBA", "round":"Välierä", "double_points": False},
     {"id":103, "date":"2026-07-18", "time":"00:00", "home":"TBA", "away":"TBA", "round":"Pronssiottelu", "double_points": True},
